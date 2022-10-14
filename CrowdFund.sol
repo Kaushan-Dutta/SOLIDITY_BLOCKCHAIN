0898ef477b66fe7b312e88d70eb37835a2028728
Kaushan Dutta <94303484+Kaushan-Dutta@users.noreply.github.com>
2022-10-14 19:06:00 +0530

    Update CrowdFund.sol

--- CrowdFund.sol
+++ CrowdFund.sol
@@ -1,6 +1,9 @@
+//creating a smart contract where the people are going to contribute in a project initiated by a manager, where the manager can only draw the money from the contract with DAO automation
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.6.0 <0.9.0;
 contract CrowdFund{
+     
+      //variables 
       mapping(address=>uint) public contributors;
       address public manager;
       uint public minimumContribution;
@@ -9,6 +12,7 @@ contract CrowdFund{
       uint public raisedAmount;
       uint public noOfContributors;
 
+       //description of the project by manager
        struct  Request{
           string description;
           address recipient;
@@ -18,6 +22,7 @@ contract CrowdFund{
           mapping(address=>bool) voters;
       }
 
+      //list of projects initiated by manager
       mapping(uint=>Request) public requests;
       uint public numRequests=0;
 
@@ -29,6 +34,7 @@ contract CrowdFund{
       }
 
       function sendEth()public payable{
+      
           require(block.timestamp<deadline,"Deadline has crossed");
           require(msg.value>=minimumContribution,"Minimum contribution is not met");
           if(contributors[msg.sender]==0){
@@ -38,9 +44,11 @@ contract CrowdFund{
           raisedAmount+=msg.value;
 
       }
+      
       function checkBalance()public view returns(uint){
           return address(this).balance;
       }
+      
       function refund()public{
           require(block.timestamp>deadline && raisedAmount<target,"Deadline not reached yet");
           require(contributors[msg.sender]>0);
@@ -49,6 +57,7 @@ contract CrowdFund{
           contributors[msg.sender]=0;
           raisedAmount-=contributors[msg.sender];
       }
+      
       
       modifier onlyManager(){
           require(msg.sender!=manager,"You are not the manager");
@@ -67,6 +76,7 @@ contract CrowdFund{
 
       } 
       function voteRequest(uint _requestNo)public{
+      
           require(contributors[msg.sender]>0,"You are not contributor");
           Request storage newRequest=requests[_requestNo];
           require(newRequest.voters[msg.sender]==false,"You have already ");
